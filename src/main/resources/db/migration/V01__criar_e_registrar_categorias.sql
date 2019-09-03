3bfa8563e6771e27a8fd658cb3be6b481ce35773
Uniliva Alves <uniliva.pereira@yaman.com.br>
2019-09-03 08:17:05 -0300

    Adicionado versionamento do banco com flyway

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,11 @@
+create table categoria(
+	codigo serial primary key,
+	nome varchar(50) NOT NULL
+);
+
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
