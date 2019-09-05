83ba874e8c2d870f1f801341876b439f0dc5e710
Uniliva Alves <uniliva.pereira@yaman.com.br>
2019-09-05 06:36:49 -0300

    Criando endpoints de categoria

---
+++ src/main/java/br/com/unidev/base/controller/CategoriaRecurso.java
@@ -0,0 +1,51 @@
+package br.com.unidev.base.controller;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.unidev.base.model.Categoria;
+import br.com.unidev.base.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaRecurso {
+
+	@Autowired
+	private CategoriaRepository repository;
+
+	@GetMapping
+	public List<Categoria> listar() {
+		return repository.findAll();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria) {
+		Categoria categoriaSalva = repository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> buscaPorCodigo(@PathVariable Integer codigo) {
+		Optional<Categoria> categoria = repository.findById(codigo);
+
+		return ResponseEntity.of(categoria);
+	}
+
+}
