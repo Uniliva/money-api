ac379edc722b005442b945070ba1be4c9df80a4b
Uniliva Alves <uniliva.pereira@yaman.com.br>
2019-09-10 07:26:34 -0300

    Adicionando eventos para adicionar cabecario Location

--- src/main/java/br/com/unidev/base/controller/PessoaRecurso.java
+++ src/main/java/br/com/unidev/base/controller/PessoaRecurso.java
@@ -1,12 +1,14 @@
 package br.com.unidev.base.controller;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.unidev.base.event.RecursoCriadoEvent;
 import br.com.unidev.base.model.Pessoa;
 import br.com.unidev.base.repository.PessoaRepository;
 
@@ -26,19 +28,19 @@ public class PessoaRecurso {
 	@Autowired
 	private PessoaRepository repository;
 
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
 	@GetMapping
 	public List<Pessoa> listar() {
 		return repository.findAll();
 	}
 
 	@PostMapping
-	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa) {
+	public ResponseEntity<Pessoa> salvar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = repository.save(pessoa);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
-
-		return ResponseEntity.created(uri).body(pessoaSalva);
-
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 
 	@GetMapping("/{codigo}")
---
+++ src/main/java/br/com/unidev/base/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,28 @@
+package br.com.unidev.base.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.unidev.base.event.RecursoCriadoEvent;
+
+@Component
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent> {
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+		Integer codigo = recursoCriadoEvent.getCodigo();
+		HttpServletResponse response = recursoCriadoEvent.getResponse();
+		adicionaLocationHeader(codigo, response);
+	}
+
+	private void adicionaLocationHeader(Integer codigo, HttpServletResponse response) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(codigo).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	}
+
+}
