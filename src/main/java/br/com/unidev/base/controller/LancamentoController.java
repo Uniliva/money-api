5c39f2a0007a76505fcf754b37f123a3e67b7009
Uniliva Alves <uniliva@hotmail.com>
2020-03-01 06:47:34 -0300

    Adicionando anotação removida

--- src/main/java/br/com/unidev/base/controller/LancamentoController.java
+++ src/main/java/br/com/unidev/base/controller/LancamentoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,7 @@ import br.com.unidev.base.repository.projection.ResumoLancamento;
 import br.com.unidev.base.service.LancamentoService;
 
 @RestController
+@RequestMapping("/lancamentos")
 public class LancamentoController {
 
 	@Autowired
