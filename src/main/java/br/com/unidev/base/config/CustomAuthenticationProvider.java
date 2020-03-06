89f9ad31e3d8645cf8835824bade4dac6da7932d
Uniliva <uniliva.pereira@yaman.com.br>
2020-03-06 09:55:51 -0300

    Ajustando login com oauth

--- src/main/java/br/com/unidev/base/config/CustomAuthenticationProvider.java
+++ src/main/java/br/com/unidev/base/config/CustomAuthenticationProvider.java
@@ -2,31 +2,46 @@ package br.com.unidev.base.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import br.com.unidev.base.service.CustomUserDatailsService;
 
 @Component
-public class CustomAuthenticationProvider  implements AuthenticationProvider {
-	
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
 	@Autowired
 	private CustomUserDatailsService customUserDetailsService;
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
 	@Override
-	public Authentication authenticate(Authentication authentication) throws AuthenticationException {		
-		UserDetails userDetails = customUserDetailsService.loadUserByUsername( authentication.getName());		
-		if (userDetails == null) return null;
-		
-		return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());	
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String email = authentication.getName();
+		String password = authentication.getCredentials().toString();
+
+		UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+		if (userDetails == null)
+			return null;
+
+		if (passwordEncoder.matches(password, userDetails.getPassword())) {
+			return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null,
+					userDetails.getAuthorities());
+		} else {
+			throw new BadCredentialsException("Usuario e/ou senha incorretos");
+		}
+
 	}
 
 	@Override
 	public boolean supports(Class<?> authentication) {
-		 return authentication.equals(UsernamePasswordAuthenticationToken.class);
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
 	}
-	
+
 }
